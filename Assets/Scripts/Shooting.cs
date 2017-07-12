e4a1dc33d0a252617b0034fbd11330499ac071e5
lucasfutch <lucasfutch@gmail.com>
2017-07-12 17:59:12 -0600

    raycast shoorting and effects

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shooting : MonoBehaviour {
+
+	public GameObject objetoAAparecer;
+	public GameObject particleEffect;
+	public GameObject toggleButton;
+
+
+	private bool canShoot;
+	private bool toggled;
+	private float timeElapsed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+		if (timeElapsed > 2) {
+			canShoot = true;
+		} else {
+			timeElapsed += Time.deltaTime;
+		}
+	}
+
+	public void changeToggle() {
+
+		if (toggleButton.GetComponent<Toggle> ().isOn) {
+			toggled = true;
+		} else {
+			toggled = false;
+		}
+	}
+
+	public void Shoot() {
+		if (toggled) {
+			if (canShoot) {
+				canShoot = false;
+				RaycastHit hit;
+	
+				if (Physics.Raycast (Camera.main.transform.position, Camera.main.transform.forward, out hit, 10000)) {
+			
+					Debug.Log (hit.collider.name);
+					if (hit.collider.CompareTag ("Destructible")) {
+						Destroy (hit.collider.gameObject);
+						Instantiate (particleEffect, hit.point, Quaternion.Euler (hit.normal));
+					} else if (hit.collider.CompareTag ("Disappear")) {
+						hit.collider.gameObject.SetActive (false);
+						Instantiate (objetoAAparecer, hit.collider.transform.position, Quaternion.identity);
+					}
+			
+				}
+			}
+
+		} else {
+			if (canShoot) {
+				canShoot = false;
+
+
+
+			}
+
+		}
+	}
+}
---
+++ Assets/destructor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destructor : MonoBehaviour {
+
+	private float timeElapsed = 0.0f;
+	public float timeForDest;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeElapsed > timeForDest) {
+			Destroy (gameObject);
+		} else {
+			timeElapsed += Time.deltaTime;
+		}
+	}
+
+
+
+}
